412dd432b81ee13011f4635e8a4017ea8d72609f
Breno Azevedo <maddox.br@gmail.com>
2024-01-04 23:59:46 -0300

    * Started updating the requirement for Configs\icontypes.lua (WIP)

--- LuaUI/Configs/icontypes.lua
+++ LuaUI/Configs/icontypes.lua
@@ -14,7 +14,15 @@
 
 local iconsPath = "LuaUI/Icons/"
 
+
+
 --- iconID, baseIconSize
+---- Eg: commanders
+--	commander0 = {
+--  	bitmap='icons/armcommander.dds',
+--  	size=1.9,
+-- 		distance=1.0,
+--	},
 local unitIconTable = {
 	air_generic=1.25,
 	air_radar=1.1,
@@ -99,7 +107,7 @@ local tierSizeMult = {
 	[4]=2,
 }
 
-local icontypes = {
+local iconTypes = {
 	default = {
 		size = 1.0,
 		radiusadjust = 1,
@@ -108,19 +116,101 @@ local icontypes = {
 		size = 0,
 		radiusadjust = 0,
 	},
+	Initialize = function () --loadUnitIcons
+		local function addUnitIcon(name, path, size)
+			if (path) then
+				Spring.AddUnitIcon(name, path, size) end
+			--iconTypes[name] = path
+			name = path
+		end
+		local function tryLoad(fileName, iconName, size)
+			if VFS.LoadFile(fileName) then
+				addUnitIcon(iconName, fileName, size)
+			else
+				Spring.Echo("Icon file not found: "..fileName)
+			end
+		end
+
+		for iconid, baseSize in pairs(unitIconTable) do
+			for tier = 0, 4 do  -- eg.: veh_tank_1.png (for a tier 1 tank)
+				local calcSize = baseSize * tierSizeMult[tier]
+				local fileName = iconsPath..iconid..".png"
+				tryLoad(fileName, iconid.."_"..tier, calcSize )
+			end
+		end
+		-- Tech Centers
+		local fileName = iconsPath.."structure_techcenter"
+		for tier = 0, 4 do  -- eg.: veh_tank_1.png (for a tier 1 tank)
+			local fileName = fileName..tier..".png"
+			tryLoad(fileName, "structure_techcenter"..tier, 1.9 )
+		end
+		-- Outposts
+		fileName = iconsPath.."structure_outpost"
+		tryLoad(fileName..".png", "structure_outpost", 1.6 )
+		tryLoad(fileName.."2.png", "structure_outpost2", 1.7 )
+		tryLoad(fileName.."3.png", "structure_outpost3", 1.8 )
+		tryLoad(fileName.."4.png", "structure_outpost4", 1.9 )
+
+		-- Commanders et al
+		addUnitIcon("armcom.user", "LuaUI/Icons/armcom.png",2)
+		addUnitIcon("corcom.user", "LuaUI/Icons/corcom.png",2)
+		addUnitIcon("krogoth.user", "LuaUI/Icons/krogoth.png",3.3)
+		addUnitIcon("bantha.user", "LuaUI/Icons/bantha.png",2.6)
+		addUnitIcon("corjugg.user", "LuaUI/Icons/juggernaut.png",3.5)
+		addUnitIcon("star.user", "LuaUI/Icons/star.png")
+		addUnitIcon("blank.user", "LuaUI/Icons/blank.png")
+
+		-- Setup the unitdef icons
+		for udid,ud in pairs(UnitDefs) do
+			if ud then
+				local tier = ud.customParams.tier or 0
+				local iconTag = ud.customParams.icontag
+				--Spring.Echo(" udid | name: "..udid.." | "..ud.name.." subs: "..ud.name:sub(0,6))
+				--      -- Icontag defined
+				--      --Spring.Echo("Unit name for icon: "..ud.name)
+				if iconTag then
+					Spring.SetUnitDefIcon(udid, iconTag.."_"..tier)
+					--Spring.Echo("Set icon: "..iconTag.."_"..tier)
+
+					-- #################
+					-- Exceptional Cases
+					-- #################
+				elseif (ud.name=="roost") or (ud.name=="meteor") then
+					Spring.SetUnitDefIcon(udid, "star.user")
+				elseif string.sub(ud.name, 0, 7) == "critter" then
+					Spring.SetUnitDefIcon(udid, "blank.user")
+				elseif ud.name == "armcom" or ud.name:sub(0,6)=="armcom" then    -- Tiers 1 through 4
+					--Spring.Echo("Commander found")
+					Spring.SetUnitDefIcon(udid, "armcom.user")
+				elseif ud.name == "corcom" or ud.name:sub(0,6)=="corcom" then    -- Tiers 1 through 4
+					--Spring.Echo("Commander found")
+					Spring.SetUnitDefIcon(udid, "corcom.user")
+				elseif ud.name == "armtech" or ud.name:sub(0,7)=="armtech" or    -- Tiers 1 through 4
+						ud.name == "cortech" or ud.name:sub(0,7)=="cortech" then
+					Spring.SetUnitDefIcon(udid, "structure_techcenter"..tier)
+				elseif ud.name == "armoutpost" or ud.name == "coroutpost" then    -- Tiers 0 (no Tier 1)
+					Spring.SetUnitDefIcon(udid, "structure_outpost")
+				elseif ud.name:sub(0,10)=="armoutpost" or ud.name:sub(0,10)=="coroutpost" then -- Tiers 2 ~ 4
+					Spring.SetUnitDefIcon(udid, "structure_outpost"..tier)
+				elseif ud.name=="armbanth" then
+					Spring.SetUnitDefIcon(udid, "bantha.user")
+				elseif ud.name=="corkrog" then
+					Spring.SetUnitDefIcon(udid, "krogoth.user")
+				elseif ud.name=="corjugg" then
+					Spring.SetUnitDefIcon(udid, "corjugg.user")
+					--else
+					--  Spring.SetUnitDefIcon(udid, "generic_unit_"..tier)
+					--  Spring.Echo("Icontag for "..ud.name.." not found, setting it to generic_unit_"..tier)
+				end
+			end
+		end
+	end
 }
-  
----- Eg: commanders
---	commander0 = {
---  	bitmap='icons/armcommander.dds',
---  	size=1.9,
--- 		distance=1.0,
---	},
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-return icontypes
+return iconTypes
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
