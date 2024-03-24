e1604ca7d37e2e358a8ec83891409c7507376df0
Breno Azevedo <maddox.br@gmail.com>
2024-03-24 14:47:05 -0300

    * Updated gfx_highlight_api_gl4.lua

--- LuaUI/Widgets/gui_highlight_unit_gl4.lua
+++ LuaUI/Widgets/gui_highlight_unit_gl4.lua
@@ -1,35 +1,31 @@
 function widget:GetInfo()
 	return {
-		name = "Highlight Unit GL4",
+		name = "Highlight Unit GL4 v2",
 		desc = "Highlights the unit or feature under the cursor",
 		author = "Floris (original: trepan)",
 		date = "January 2022",
 		license = "GNU GPL, v2 or later",
-		layer = -10000,
-		enabled = false, --true  --  loaded by default?
+		layer = 5,
+		enabled = true
 	}
 end
 
-local hideBelowGameframe = 100
-
-local highlightAlpha = 0.1
-local selectedHighlightAlpha = 0.09
-
-local edgeAlpha = 1
-local selectedEdgeAlpha = 0.75
-
+local hideBelowGameframe = 5
+local edgeAlpha = 0.4
 local edgeExponent = 1.2
-local selectedEdgeExponent = 1.45
 
-local animationAlpha = 0.7
-local selectedAnimationAlpha = 0.5
-
-local useTeamcolor = true
+local useTeamcolor = false
 local teamColorAlphaMult = 1.25
 local teamColorMinAlpha = 0.7
+local fadeTime = 0.05
+
+local myUnitColor    = {1, 1, 1}
+local allyUnitColor  = {1, 1, 1}
+local enemyUnitColor = {1, 1, 1}
 
 local vsx, vsy = Spring.GetViewGeometry()
 
+local hidden = (Spring.GetGameFrame() <= hideBelowGameframe)
 local selectedUnits = Spring.GetSelectedUnits()
 local unitIsSelected = false
 
@@ -37,145 +33,202 @@ local spGetMouseState = Spring.GetMouseState
 local spTraceScreenRay = Spring.TraceScreenRay
 local spGetUnitTeam = Spring.GetUnitTeam
 
--- Beherith says: There can only be one!
-local highlightunitID = nil
-local highlightID = nil
+local unitshapes = {}
+local fadeUnits = {}
 
-local visibleUnits = {} -- table maintained based on unit tracker API calls
-
-local teamColor = {}
-local teams = Spring.GetTeamList()
-for i = 1, #teams do
-	local r, g, b = Spring.GetTeamColor(teams[i])
-	local min = teamColorMinAlpha
-	teamColor[teams[i]] = { math.max(r, min), math.max(g, min), math.max(b, min) }
-end
-teams = nil
-
-local function isUnitSelected(unitID)
-	for i, selUnitID in ipairs(selectedUnits) do
-		if selUnitID == unitID then
-			return true
-		end
-	end
-	return false
-end
-
-local function removeUnitShape()
-	if not WG.StopHighlightUnitGL4 then
-		widget:Shutdown("No API")
-	elseif highlightunitID and highlightID then
-		WG.StopHighlightUnitGL4(highlightID)
-		highlightunitID = nil
-		highlightID = nil
-	end
-end
-
-local function addUnitShape(unitID)
-	if (not Spring.ValidUnitID(unitID)) or (Spring.GetUnitIsDead(unitID) ~= false) or (visibleUnits[unitID] == nil) then
-		-- remove old and bail
-		removeUnitShape()
-		return
-	end
-	if not WG.HighlightUnitGL4  then
-		widget:Shutdown("No API")
+local function UpdateTeamColors()
+	local saturation = (options and options.highlightSat.value) or 0.5
+	allyUnitColor = (WG.LocalColor or {}).allyColor or {1, 1, 1}
+	if options and options.selfHoverSameAsAllies.value then
+		myUnitColor = allyUnitColor
 	else
-		local r,g,b
-		unitIsSelected = isUnitSelected(unitID)
-		local a = unitIsSelected and selectedHighlightAlpha or highlightAlpha
-		if useTeamcolor then
-			local teamID = spGetUnitTeam(unitID)
-			if teamID then
-				r, g, b = teamColor[teamID][1], teamColor[teamID][2], teamColor[teamID][3]
-				a = a * teamColorAlphaMult
+		myUnitColor = (WG.LocalColor or {}).myColor or {1, 1, 1}
+	end
+	enemyUnitColor = (WG.LocalColor or {}).enemyColor or {1, 1, 1}
+	allyUnitColor = {
+		allyUnitColor[1]*saturation + (1 - saturation),
+		allyUnitColor[2]*saturation + (1 - saturation),
+		allyUnitColor[3]*saturation + (1 - saturation),
+	}
+	myUnitColor = {
+		myUnitColor[1]*saturation + (1 - saturation),
+		myUnitColor[2]*saturation + (1 - saturation),
+		myUnitColor[3]*saturation + (1 - saturation),
+	}
+	enemyUnitColor = {
+		enemyUnitColor[1]*saturation + (1 - saturation),
+		enemyUnitColor[2]*saturation + (1 - saturation),
+		enemyUnitColor[3]*saturation + (1 - saturation),
+	}
+	Spring.Echo("allyUnitColor", allyUnitColor[1], allyUnitColor[2], allyUnitColor[3])
+	Spring.Echo("myUnitColor", myUnitColor[1], myUnitColor[2], myUnitColor[3])
+	Spring.Echo("enemyUnitColor", enemyUnitColor[1], enemyUnitColor[2], enemyUnitColor[3])
+end
+
+options_path = 'Settings/Graphics/Unit Visibility'
+options_order = { 'highlightStrength', 'highlightSat', 'selfHoverSameAsAllies'}
+options = {
+	highlightStrength = {
+		name = "Mouse hover highlight strength",
+		type = "number",
+		value = 0.35,
+		min = 0,
+		max = 1,
+		step = 0.01,
+	},
+	highlightSat = {
+		name = "Mouse hover highlight saturation",
+		type = "number",
+		value = 0.5,
+		min = 0,
+		max = 1,
+		step = 0.01,
+		OnChange = UpdateTeamColors
+	},
+	selfHoverSameAsAllies = {
+		name = 'Use ally hover cover for self',
+		type = 'bool',
+		value = false,
+		OnChange = UpdateTeamColors
+	},
+}
+
+local function GetHighlightColorForUnit(unitID)
+	local spectating = Spring.GetSpectatingState()
+	if (not spectating) and (Spring.GetUnitTeam(unitID) == Spring.GetMyTeamID()) then
+		return myUnitColor[1], myUnitColor[2], myUnitColor[3]
+	end
+	if (Spring.GetUnitAllyTeam(unitID) == Spring.GetMyAllyTeamID()) then
+		return allyUnitColor[1], allyUnitColor[2], allyUnitColor[3]
+	end
+	return enemyUnitColor[1], enemyUnitColor[2], enemyUnitColor[3]
+end
+
+local function AddUnitShape(unitID)
+	if not WG.HighlightUnitGL4 or not Spring.ValidUnitID(unitID) then
+		widget:Shutdown()
+	else
+		local r, g, b = GetHighlightColorForUnit(unitID)
+		local mult = 1
+		if not unitshapes[unitID] then
+			fadeUnits[unitID] = os.clock()
+			mult = 0.13
+		elseif fadeUnits[unitID] then
+			if fadeUnits[unitID] > 0 then
+				mult = 0.05 + (os.clock() - fadeUnits[unitID]) / fadeTime + ((1/Spring.GetFPS())/fadeTime)
+				if mult >= 1 then
+					mult = 1
+					fadeUnits[unitID] = nil
+				end
+			else
+				mult = 1 - ((os.clock() - math.abs(fadeUnits[unitID])) / fadeTime)
+				if mult <= 0 then
+					fadeUnits[unitID] = nil
+				end
 			end
 		end
-
-		if highlightunitID or highlightID then
-			removeUnitShape()
+		if unitshapes[unitID] then
+			WG.StopHighlightUnitGL4(unitshapes[unitID])
+			unitshapes[unitID] = nil
 		end
-
-		highlightunitID = unitID
-		highlightID =  WG.HighlightUnitGL4(unitID, 'unitID', r,g,b, a,
-				(unitIsSelected and selectedEdgeAlpha or edgeAlpha),
-				(unitIsSelected and selectedEdgeExponent or edgeExponent),
-				(unitIsSelected and selectedAnimationAlpha or animationAlpha),
-				0,0,0,0,"mouseover")
-		return highlightID
+		if mult > 0 then
+			unitshapes[unitID] = WG.HighlightUnitGL4(unitID, 'unitID', r,g,b, 0, options.highlightStrength.value*mult, edgeExponent, 0)
+			return unitshapes[unitID]
+		end
 	end
 end
 
-
-local function processSelection()
-	local prevUnitIsSelected = unitIsSelected
-	unitIsSelected = isUnitSelected(highlightunitID)
-	if highlightunitID and unitIsSelected ~= prevUnitIsSelected then
-		addUnitShape(highlightunitID)
+local function RemoveUnitShape(unitID, force)
+	if not WG.StopHighlightUnitGL4 then
+		widget:Shutdown()
+	elseif unitID and unitshapes[unitID] then
+		if force then
+			WG.StopHighlightUnitGL4(unitshapes[unitID])
+			unitshapes[unitID] = nil
+			fadeUnits[unitID] = nil
+		elseif not fadeUnits[unitID] then
+			fadeUnits[unitID] = -os.clock()
+		elseif fadeUnits[unitID] and fadeUnits[unitID] > 0 then
+			local mult = 1 - ((os.clock() - math.abs(fadeUnits[unitID])) / fadeTime)
+			fadeUnits[unitID] = -(os.clock() - (fadeTime * mult))
+		end
 	end
 end
 
-function widget:SelectionChanged(sel)
-	selectedUnits = sel
-	processSelection()
-end
-
-
-function widget:VisibleUnitAdded(unitID, unitDefID, unitTeam) -- E.g. when a unit dies
-	visibleUnits[unitID] = unitDefID
-end
-
-function widget:VisibleUnitRemoved(unitID) -- E.g. when a unit dies
-	if highlightunitID == unitID then
-		removeUnitShape()
+local function ClearUnitshapes(keepUnitID, force)
+	for unitID, _ in pairs(unitshapes) do
+		if not keepUnitID or unitID ~= keepUnitID then
+			RemoveUnitShape(unitID, force)
+		end
 	end
-	visibleUnits[unitID] = nil
 end
 
-function widget:VisibleUnitsChanged(extVisibleUnits, extNumVisibleUnits)
-	-- Called when players are changed, better remove all of them now!
-	visibleUnits = {}
-	for unitID, unitDefID in pairs(extVisibleUnits) do
-		visibleUnits[unitID] = unitDefID
+function widget:UnitDestroyed(unitID)
+	if unitshapes[unitID] then
+		RemoveUnitShape(unitID, true)
 	end
-	removeUnitShape()
 end
 
 function widget:ViewResize()
 	vsx, vsy = Spring.GetViewGeometry()
 end
 
-function widget:Update()
-	if WG.StopHighlightUnitGL4 then
-		local mx, my, p1, mmb, _, mouseOffScreen, cameraPanMode  = spGetMouseState()
-		if mouseOffScreen or cameraPanMode or mmb or p1 then
-			removeUnitShape()
-		else
-			local type, data = spTraceScreenRay(mx, my)
-			local unitID
-			if type == 'unit' and not Spring.IsGUIHidden() then
-				unitID = data
-				if highlightunitID ~= unitID then
-					addUnitShape(unitID)
-				end
-			else
-				removeUnitShape()
-			end
+local function ShouldHighlight()
+	if Spring.IsGUIHidden() then
+		return false
+	end
+	local screen0 = WG.Chili and WG.Chili.Screen0
+	if screen0 and screen0.activeControl then
+		return false
+	end
+	if screen0 and screen0.hoveredControl then
+		return false
+	end
+	return true
+end
 
+function widget:Update()
+	if hidden and Spring.GetGameFrame() > hideBelowGameframe then
+		hidden = false
+	end
+	if WG.StopHighlightUnitGL4 then
+		local mx, my, lmb, mmb, rmb, outsideSpring = spGetMouseState()
+		if outsideSpring or options.highlightStrength.value == 0 then
+			ClearUnitshapes(nil, true)
+		else
+			local targetType, data = spTraceScreenRay(mx, my)
+			local unitID
+			local addedUnitID
+			if targetType == 'unit' and ShouldHighlight() then
+				unitID = data
+				if not unitshapes[unitID] then
+					AddUnitShape(unitID)
+					addedUnitID = unitID
+				end
+			end
+			ClearUnitshapes(unitID)
+
+			for unitID, v in pairs(fadeUnits) do
+				if unitID ~= addedUnitID then
+					AddUnitShape(unitID)
+				end
+			end
 		end
 	end
 end
 
 function widget:Initialize()
-	if (not WG.HighlightUnitGL4) or (not WG['unittrackerapi']) then
-		Spring.Echo("Highlight Unit GL4 requires WG.HighlightUnitGL4 and WG.unittrackerapi")
+	if not WG.HighlightUnitGL4 then
 		widgetHandler:RemoveWidget()
+		return
 	end
-	widget:VisibleUnitsChanged(WG['unittrackerapi'].visibleUnits, nil)
+	WG.LocalColor.RegisterListener("HighlightUnitGl4", UpdateTeamColors)
+	UpdateTeamColors()
 end
 
-function widget:Shutdown(reason)
-	Spring.Echo("Highlight Unit Widget Exiting", reason)
+function widget:Shutdown()
 	if WG.StopHighlightUnitGL4 then
-		removeUnitShape()
+		ClearUnitshapes(false, true)
+		WG.LocalColor.UnregisterListener("HighlightUnitGl4")
 	end
 end
